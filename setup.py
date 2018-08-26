94624efd673c0478a6a22f929891f7e2cdda6beb
Rob McBroom <github@skurfer.com>
2018-08-26 15:28:12 -0400

    packaging improvements

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 setup(
     name="ldaprc",
-    version="1.0.0",
+    version="1.0.1",
     author='Rob McBroom',
     author_email='python-ldaprc@skurfer.com',
     url='https://github.com/skurfer/ldaprc',
     py_modules=['ldaprc'],
+    description='Library for reading OpenLDAP settings in Python',
+    keywords='configuration settings ldap3 ldap openldap',
 )
